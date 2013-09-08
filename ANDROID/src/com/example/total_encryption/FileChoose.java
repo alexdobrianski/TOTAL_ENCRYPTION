5d7a92959491723adcecdf32b5eea8190cae7a72
alex dobrianski <adobri@shaw.ca>
2013-09-08 00:00:31 -0700

    two activities (main +FileCho0se dialog)

---
+++ ANDROID/src/com/example/total_encryption/FileChoose.java
@@ -0,0 +1,22 @@
+package com.example.total_encryption;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class FileChoose extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_file_choose);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.file_choose, menu);
+		return true;
+	}
+
+}
