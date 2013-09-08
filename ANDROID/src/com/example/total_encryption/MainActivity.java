06226cd3f597b45b355ddcc1b3705290df52d0ab
alex dobrianski <adobri@shaw.ca>
2013-09-07 20:36:03 -0700

    copy right

--- ANDROID/src/com/example/total_encryption/MainActivity.java
+++ ANDROID/src/com/example/total_encryption/MainActivity.java
@@ -1,3 +1,19 @@
+/*
+ * 2013 (C) Alex Dobrianski Satellite encryption communication
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>
+    Design and development by Team "Plan B" is licensed under 
+    a Creative Commons Attribution-ShareAlike 3.0 Unported License.
+    http://creativecommons.org/licenses/by-sa/3.0/ 
+ */
 package com.example.total_encryption;
 
 import android.net.Uri;
@@ -30,7 +46,7 @@ public class MainActivity extends Activity {
             public void onClick(View v) 
             {
              	/*
-			    SetUserNames();
+			    	SetUserNames();
 			    CString FileName;
 			    m_EncryptKey.GetWindowTextW(FileName);
 			    CStringA FileNameA = (CStringA)FileName;
